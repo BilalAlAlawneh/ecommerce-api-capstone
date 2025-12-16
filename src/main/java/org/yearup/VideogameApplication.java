acbbd95a96251d8e1c3ee2c1543de6d9cbbe1aca
BilalAlAlawneh <bilalalalawneh@gmail.com>
2025-12-15 19:53:36 -0800

    V1) uploaded and configured

---
+++ src/main/java/org/yearup/VideogameApplication.java
@@ -0,0 +1,14 @@
+package org.yearup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideogameApplication
+{
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideogameApplication.class, args);
+    }
+
+}
